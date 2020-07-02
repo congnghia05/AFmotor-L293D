30aa2ef08d05ee95949261d80c2227da67c21b37
hockel <837316354@qq.com>
2020-07-02 20:19:22 +0800

    更新

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# I2C OLED-2864 显示屏
+# AFMotor 电机驱动扩展
 
 
 ![](./arduinoC/_images/featured.png)
@@ -8,47 +8,82 @@
 ## 目录
 
 * [相关链接](#相关链接)
-* [描述](#描述)
+* [支持列表](#支持列表)
+* [介绍](#介绍)
 * [积木列表](#积木列表)
 * [示例程序](#示例程序)
 * [许可证](#许可证)
-* [支持列表](#支持列表)
 * [更新记录](#更新记录)
 
 ## 相关链接
-* 本项目加载链接: ```https://github.com/DFRobot/ext-oled12864```
-
+* 本项目加载链接: ```https://gitee.com/hockel/AFMotor```
+* 4*4 矩阵键盘链接：```https://gitee.com/hockel/Keypad```
 * 用户库教程链接: ```https://mindplus.dfrobot.com.cn/extensions-user```
+* 购买此产品: [某宝]([https://s.taobao.com/search?q=%E5%85%BC%E5%AE%B9Arduino+%E7%94%B5%E6%9C%BA%E9%A9%B1%E5%8A%A8%E6%89%A9%E5%B1%95%E6%9D%BF+motor+control+shield+L293D+%E9%A9%AC%E8%BE%BE%E6%9D%BF&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20200702&ie=utf8](https://s.taobao.com/search?q=兼容Arduino+电机驱动扩展板+motor+control+shield+L293D+马达板&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20200702&ie=utf8)).
 
-* 购买此产品: [商城](https://www.dfrobot.com.cn/).
+## 支持列表
 
-## 描述
-支持屏幕上的不同图形，包括点，线，圆，矩形，三角形，圆角矩形和一些命令。
-支持DFRobot模块的英文字符和字符库芯片。
-IIC：ESP系列控制器重绘时重绘速率超过每秒30帧
+| 主板型号  | 实时模式 | ArduinoC | MicroPython | 备注 |
+| --------- | :------: | :------: | :---------: | ---- |
+| micro:bit |          |    √     |             |      |
+| mpython   |          |    √     |             |      |
+
+## 介绍
+
+#### AFMOTOR电机扩展板主要功能特点：
+
+- 可最多驱动4个模型直流电机
+- 可最多驱动2个28BYJ-48步进电机
+- 可最多驱动2个舵机（伺服电机）
+- 板载两块L293D芯片负责电机驱动的核心工作
+- 可配合Arduino Uno/Mega开发板使用（扩展主要针对Uno开发板进行开发）
+
+#### 外接电源电压要求
+
+AFMotor扩展板中起关键作用的芯片是L293D。该芯片所允许的直流电源电压范围是4.5V ～25V。因此AFMotor扩展板外接电源接口允许我们连接的电源也是4.5V ～25V。请注意：这是一个很宽泛的电压指标。具体我们应该连接的电源电压有多大，这是由被驱动的电机工作电压来决定的。
+
+#### 工作运行时AFMOTOR电机扩展板使用方法：
+
+一般在正常工作状态下，Arduino开发板是不会与电脑通过数据线进行连接的。这时我们就要通过AFMotor电机扩展板上外接电源端口同时为扩展板和Arduino供电。
+
+***注意：当使用外接电源同时为Arduino和电机扩展板供电时，扩展板上的跳线必须安置在插针上。否则Arduino是无法工作的。***
+
+#### 扩展板跳线的作用
+
+在以上两种工作方式里，我们对电机扩展板上的跳线采取了不同的操作方法。该跳线的作用是控制Arduino开发板是否通过外接电源来供电。当跳线移除的时候，外接电源不会为Arduino开发板供电。相反当跳线安置在插针上，外接电源将会为Arduino供电。
+
+因此在程序开发过程中，当Arduino与电脑通过USB线连接时，我们需要将跳线从插针上移除。假如我们不移除该跳线，Arduino会有两路供电。而这两路电源同时工作会产生问题，严重的可能会造成开发板以及扩展板的损坏。
+
+​                                                                                                                                                        (--转自[太极创客网站](http://www.taichi-maker.com/homepage/reference-index/motor-reference-index/arduino-motor-shield/)）
 
 ## 积木列表
 
 ![](./arduinoC/_images/blocks.png)
 
-
-
 ## 示例程序
 
+###  示例一、驱动四个电机的运动
+
 ![](./arduinoC/_images/example.png)
+
+
+
+### 示例二、驱动步进电机程序
+
+#### 电路连接图：
+
+![](./arduinoC/_images/temp1.png)
+
+#### 程序代码：
+
+![](./arduinoC/_images/example1.png)
 
 ## 许可证
 
 MIT
 
-## 支持列表
-
-主板型号                | 实时模式    | ArduinoC   | MicroPython    | 备注
------------------- | :----------: | :----------: | :---------: | -----
-micro:bit        |             |       √       |             | 
-mpython        |             |        √      |             | 
-
 
 ## 更新日志
 * V0.0.1  基础功能完成
-* V0.0.2  根据Mind+V1.6.2 RC2.0更新此库
+
+  
--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -1,11 +1,3 @@
-
-enum SIZE {
-    //% block="29*29"
-    1,
-    //% block="58*58"
-    2
-}
-
 enum LINE {
     //% block="M1"
     1,
@@ -23,6 +15,22 @@ enum DIR {
     //% block="后退"
     BACKWARD
 }
+enum COM {
+    //% block="M1M2"
+    1,
+    //% block="M3M4"
+    2
+}
+enum MODEL {
+    //% block="全步进(单线圈)"
+    SINGLE,
+    //% block="全步进(双线圈)"
+    DOUBLE,
+    //% block="半步进"
+    INTERLEAVE,
+    //% block="微步进"
+    MICROSTEP
+}
 
 
 //% color="#3264C8" iconWidth=50 iconHeight=40
@@ -35,17 +43,103 @@ namespace AFMOTOR {
         let Motor = parameter.motor.code;
         let Dir = parameter.dir.code;
         let Speed = parameter.speed.code;
-        Generator.addInclude('AFMOTORINIT', '#include <AFMotor.h>');
+        Generator.addInclude('AFMOTOR_INIT', '#include <AFMotor.h>');
         Generator.addObject(`AFMOTORobj${Motor}`, `AF_DCMotor`, `motor${Motor}(${Motor});`,true);
         Generator.addCode(`motor${Motor}.setSpeed(${Speed});\n\tmotor${Motor}.run(${Dir});`);
     }
-    
     //% block="停止电机 [motor]" blockType="command"
     //% motor.shadow="dropdown" motor.options="LINE" BUTTON.defl="LINE.M1"
     export function AFMotorStop(parameter: any, block: any) {
         let Motor = parameter.motor.code;
         Generator.addCode(`motor${Motor}.setSpeed(0);\n\t motor${Motor}.run(RELEASE);`);
     }
-   
+    //% block="设置步进电机步数 [s1] 端口[c1] 运转模式[m1]" blockType="command"
+    //% s1.shadow="number" s1.defl="2048" 
+    //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    //% m1.shadow="dropdown" m1.options="MODEL"
+    export function AFMotorStepperinit(parameter: any, block: any) {
+        let S1 = parameter.s1.code;
+        let C1 = parameter.c1.code;
+        let M1 = parameter.m1.code;
+        Generator.addInclude('AFMOTOR_INIT', '#include <AFMotor.h>');
+        Generator.addInclude('STEPPER_INIT', '#include <AccelStepper.h>');
+        Generator.addObject(`Af_Stepper${C1}`, `AF_Stepper`, `motor${C1}(${S1}, ${C1});`);
+        Generator.addObject(`forwardstep${C1}`, `void`, `forwardstep${C1}() {\n\tmotor${C1}.onestep(FORWARD, ${M1});\n\t}`);
+        Generator.addObject(`backwardstep${C1}`, `void`, `backwardstep${C1}() {\n\tmotor${C1}.onestep(BACKWARD, ${M1});\n\t}`);
+        Generator.addObject(`Accel_Stepper${C1}`, `AccelStepper`, `stepper${C1}(forwardstep${C1}, backwardstep${C1});`);
+        
+    }
+    //% block="设置步进电机[c1]最大速度 [speed]" blockType="command"
+    //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    //% speed.shadow="number" speed.defl="500"
+    export function MaxSpeedset(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        let S1 = parameter.speed.code;
+        Generator.addCode(`stepper${C1}.setMaxSpeed(${S1});	`);
+    }
+    //% block="设置电机[c1]运行速度 [speed]" blockType="command"
+    //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    //% speed.shadow="number" speed.defl="300"
+    export function SpeedInit(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        let S1 = parameter.speed.code;
+        Generator.addCode(`stepper${C1}.setSpeed(${S1});`);
+    }
+    //% block="步进电机[c1]加速度[a1]" blockType="command"
+     //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+     //% a1.shadow="number"  a1.defl="50"
+     export function stepperAcceleration(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        let A1 = parameter.a1.code;
+        Generator.addCode(`stepper${C1}.setAcceleration(${A1});`);
+    }
+    //% block="步进电机[c1]移动目标相对位置[p1]" blockType="command"
+    //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    //% p1.shadow="number"  p1.defl="100"
+     export function stepperMove(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        let P1 = parameter.p1.code;
+        Generator.addCode(`stepper${C1}.move(${P1});`);
+    }
+    //% block="步进电机[c1]移动目标绝对位置[p1]" blockType="command"
+    //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    //% p1.shadow="number"  p1.defl="100"
+    export function stepperMoveTo(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        let P1 = parameter.p1.code;
+        Generator.addCode(`stepper${C1}.moveTo(${P1});`);
+    }
+    //% block="获取步进电机[c1]运行当前位置" blockType="reporter"
+    //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    export function stepperCurrentPosition(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        Generator.addCode(`stepper${C1}.currentPosition()`);
+    }
+    //% block="获取步进电机[c1]运行目标位置" blockType="reporter"
+    //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    export function steppertargetPosition(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        Generator.addCode(`stepper${C1}.targetPosition()`);
+    }
+
+
+    //% block="步进电机[c1]运行(匀速模式)" blockType="command"
+     //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    export function stepperStart(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        Generator.addCode(`stepper${C1}.runSpeed();`);
+    }
+    //% block="步进电机[c1]运行(先加速后减速模式)" blockType="command"
+     //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+    export function stepperRunStart(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        Generator.addCode(`stepper${C1}.run();`);
+    }
+    //% block="停止步进电机[c1]" blockType="command"
+     //% c1.shadow="dropdown" c1.options="COM" c1.defl="COM.M1M2"
+     export function stepperStop(parameter: any, block: any) {
+        let C1 = parameter.c1.code;
+        Generator.addCode(`stepper${C1}.stop();`);
+    }
 
 }
